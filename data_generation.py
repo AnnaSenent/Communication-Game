4c49e2f622e2ee58bf5f0687cd695319ba021028
Anna Senent <annasenju@gmail.com>
2022-05-01 23:31:10 +0200

    update data_generation.py

--- data_generation.py
+++ data_generation.py
@@ -1,20 +1,19 @@
+"""
+This script generates a data set in the format expected by the sum game
+"""
 
 import numpy as np
 
 np.random.seed(123)
 
 N = 20
-n_input = 100
-n_integers_to_sum = 2
+N_INPUT = 1000
+N_INTEGERS = 2
 
-integers = np.random.randint(0, N, n_input * n_integers_to_sum)
-integers_to_matrix = np.reshape(integers, (n_input, n_integers_to_sum))
+integers = np.random.randint(0, N + 1, N_INPUT * N_INTEGERS)
+integers_to_matrix = np.reshape(integers, (N_INPUT, N_INTEGERS))
 labels = np.sum(integers_to_matrix, axis=1)
 
 # Save to txt file
-
-file = open('data/dataset.txt', 'w')
-for integers, label in zip(integers_to_matrix, labels):
-    data = np.concatenate((integers, np.reshape(label, (1,)))).reshape((1, 3))
-    np.savetxt(file, data, fmt='%d')
-file.close()
+data = np.hstack([integers_to_matrix, labels.reshape(-1, 1)])
+np.savetxt('data/dataset.txt', data, fmt='%d')
